4b7db5ab502ae7ea1907a4c255345e89d3f37249
Towhid Islam <m.towhid.islam@gmail.com>
2025-09-04 21:27:43 +0200

    feat: adding transaction-stack impl

---
+++ src/main/java/com/infoworks/utils/transaction/TransactionManager.java
@@ -0,0 +1,95 @@
+package com.infoworks.utils.transaction;
+
+import com.infoworks.objects.Message;
+import com.infoworks.tasks.Task;
+import com.infoworks.tasks.stack.StackManager;
+import com.infoworks.tasks.stack.StackedTaskStateListener;
+
+import java.util.concurrent.TimeUnit;
+
+public class TransactionManager implements StackManager {
+
+    private StackedTaskStateListener listener;
+
+    public TransactionManager() {}
+
+    public TransactionManager(StackedTaskStateListener listener) {
+        this.listener = listener;
+    }
+
+    public StackedTaskStateListener getListener() {
+        return listener;
+    }
+
+    public void setListener(StackedTaskStateListener listener) {
+        this.listener = listener;
+    }
+
+    @Override @SuppressWarnings("Duplicates")
+    public void start(Task task, Message message) {
+        //
+        if (getListener() != null)
+            getListener().before(task, StackManager.State.Forward);
+        //Call Execute:
+        Message result = null;
+        boolean mustAbort = false;
+        try {
+            result = task.execute(message);
+        }catch (RuntimeException e) {
+            mustAbort = true;
+            result = new Message();
+            result.setPayload(String.format("{\"error\":\"%s\", \"status\":500}", e.getMessage()));
+        }
+        //End Execute:
+        if (getListener() != null)
+            getListener().after(task, State.Forward);
+        //
+        if (mustAbort){
+            stop(task, result);//ABORT-SEQUENCE:
+        }else {
+            if (task.next() == null){
+                if (getListener() != null) {
+                    Message converted = task.convert(result);
+                    getListener().finished(converted);//TERMINATION:
+                }
+            }else{
+                Message converted = task.convert(result);
+                start(task.next(), converted);//START-NEXT:
+            }
+        }
+        //
+    }
+
+    @Override
+    public void stop(Task task, Message reason) {
+        //
+        if (getListener() != null)
+            getListener().before(task, State.Backward);
+        //Call Abort:
+        Message result = null;
+        try {
+            result = task.abort(reason);
+        }catch (RuntimeException e) {}
+        //End Abort:
+        if (getListener() != null)
+            getListener().after(task, State.Backward);
+        //
+        if (task.next() == null){
+            if (getListener() != null)
+                getListener().failed(result);
+        }else{
+            stop(task.next(), result);
+        }
+        //
+    }
+
+    @Override
+    public void terminateRunningTasks(long timeout, TimeUnit unit) {}
+
+    @Override
+    public void close() throws Exception {
+        terminateRunningTasks(0l, TimeUnit.SECONDS);
+        this.listener = null;
+    }
+
+}
---
+++ src/main/java/com/infoworks/utils/transaction/TransactionStack.java
@@ -0,0 +1,158 @@
+package com.infoworks.utils.transaction;
+
+import com.infoworks.objects.Message;
+import com.infoworks.tasks.Task;
+import com.infoworks.tasks.stack.StackManager;
+import com.infoworks.tasks.stack.StackedTaskStateListener;
+import com.infoworks.tasks.stack.TaskCompletionListener;
+import com.infoworks.tasks.stack.TaskStack;
+
+import java.util.Stack;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
+
+public class TransactionStack implements TaskStack, StackedTaskStateListener {
+
+    private final StackManager manager;
+    private Stack<Task> beanStack;
+    private final Stack<Task> passedStack;
+    private State state = State.None;
+    private BiConsumer<Message, State> callback;
+    private TaskCompletionListener listener;
+
+    public TransactionStack() {
+        this.manager = new TransactionManager(this);
+        this.beanStack = new Stack<>();
+        this.passedStack = new Stack<>();
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public synchronized void cancel(){
+        if (state == State.Running){
+            state = State.Canceled;
+            manager.terminateRunningTasks(0l, TimeUnit.SECONDS);
+            if (passedStack.isEmpty()) return;
+            manager.stop(passedStack.peek(), null);
+        }
+    }
+
+    public synchronized TaskStack push(Task task){
+        if (state == State.Running) return this;
+        if (!beanStack.empty()){
+            Task top = beanStack.peek();
+            task.linkedTo(top);
+        }
+        beanStack.push(task);
+        return this;
+    }
+
+    public synchronized void commit(boolean reverse, BiConsumer<Message, State> onComplete){
+        this.callback = onComplete;
+        commit(reverse);
+    }
+
+    public synchronized void commit(boolean reverse, TaskCompletionListener onComplete) {
+        this.listener = onComplete;
+        commit(reverse);
+    }
+
+    protected void commit(boolean reverse) {
+        if (state == State.Running) return;
+        if (beanStack.isEmpty()) return;
+        if (reverse) {
+            beanStack = reverseStack(beanStack);
+        }//end of reverse
+        state = State.Running;
+        manager.start(beanStack.peek(), null);
+    }
+
+    private Stack<Task> reverseStack(Stack<Task> beanStack) {
+        //Reverse the beanStack order:
+        Stack<Task> rvStack = new Stack<>();
+        while (!beanStack.empty()){
+            Task cItem = beanStack.pop();
+            if (!rvStack.empty()){ //linking previous task as next task, Because we are going forward.
+                Task top = rvStack.peek();
+                cItem.linkedTo(top);
+            }
+            if (rvStack.isEmpty()){ //marking the end of stack.
+                cItem.linkedTo(null);
+            }
+            rvStack.push(cItem);
+        }
+        return rvStack;
+    }
+
+    @Override
+    public void before(Task task, StackManager.State state) {
+        synchronized (this){
+            if (state == StackManager.State.Forward){
+                Task popped = beanStack.pop();
+                passedStack.push(popped);
+            }else if (state == StackManager.State.Backward){
+                Task popped = passedStack.pop();
+                beanStack.push(popped);
+            }
+        }
+    }
+
+    @Override
+    public void after(Task task, StackManager.State state) {
+        synchronized (this){
+            if (state == StackManager.State.Forward){
+                //TODO:
+            }else if (state == StackManager.State.Backward){
+                if (passedStack.isEmpty()) {
+                    task.linkedTo(null);
+                    return;
+                }
+                Task passedTop = passedStack.peek();
+                task.linkedTo(passedTop);
+            }
+        }
+    }
+
+    @Override
+    public void failed(Message reason) {
+        synchronized (this){
+            state = State.Failed;
+        }
+        //
+        try {
+            manager.close();
+        } catch (Exception e) {}
+        //
+        try {
+            if (callback != null){
+                callback.accept(reason, state);
+            }else if (listener != null){
+                listener.failed(reason);
+            }
+        } catch (Exception e) {}
+    }
+
+    @Override
+    public void finished(Message results) {
+        synchronized (this){
+            state = State.Finished;
+            beanStack.clear();
+            passedStack.clear();
+        }
+        //
+        try {
+            manager.close();
+        } catch (Exception e) {}
+        //
+        try {
+            if (callback != null){
+                callback.accept(results, state);
+            }else if (listener != null){
+                listener.finished(results);
+            }
+        } catch (Exception e) {}
+    }
+
+}
