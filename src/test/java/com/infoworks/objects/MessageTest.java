249e5dbf3ba0ef0b76127f5bed2055d51600a006
Towhid Islam <m.towhid.islam@gmail.com>
2025-09-04 13:27:23 +0200

    test: unit test for externalizable interface for serialize and deserialize in conjunction with Redis and other cacheing tools.

--- src/test/java/com/infoworks/objects/MessageTest.java
+++ src/test/java/com/infoworks/objects/MessageTest.java
@@ -1,8 +1,10 @@
 package com.infoworks.objects;
 
+import com.infoworks.utils.MessageMapper;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.*;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -69,11 +71,62 @@ public class MessageTest {
         System.out.println(data);
     }
 
+    @Test
+    public void externalizationTest() {
+        //Since MyMessage does not have empty public constructor
+        // following will raise exception.
+        /*MyMessage message = new MyMessage();
+        message.setName("Muller");
+        message.setAge(23);*/
+        //
+        MyExtendedMessage message = new MyExtendedMessage();
+        message.setName("David");
+        message.setAge(30);
+        message.setFirstName("Md.");
+        message.setSecondName("Bishop");
+
+        /**
+         * 3. Important Notes
+         * Your Externalizable class must have a public no-arg constructor, or readExternal() will fail.
+         * writeExternal and readExternal must write and read fields in the same order.
+         * You can simulate edge cases like missing fields or partial reads by corrupting the byte stream in the test.
+         */
+
+        // Serialize to byte array
+        byte[] data = new byte[0];
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+            oos.writeObject(message);
+            data = bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Deserialize from byte array
+        MyMessage deserialized = null;
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(data);
+             ObjectInputStream ois = new ObjectInputStream(bis)) {
+            deserialized = (MyMessage) ois.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Assert values
+        Assert.assertEquals(message.getName(), deserialized.getName());
+        Assert.assertEquals(message.getAge(), deserialized.getAge());
+        System.out.println(MessageMapper.printString(deserialized));
+    }
+
 }
 
 class MyExtendedMessage extends MyMessage {
     private String firstName;
     private String secondName;
+
+    /* to test Externalizable*/
+    public MyExtendedMessage() {}
 
     public String getFirstName() {
         return firstName;
@@ -97,6 +150,12 @@ class MyMessage extends Message {
     private int age;
     private LocalDateTime dob;
     private Boolean isActivate;
+
+    /**
+     * Uncomment to test the effect of Externalizable,
+     * during serialization/readExternal()
+     */
+    //public MyMessage() {}
 
     public String getName() {
         return name;
