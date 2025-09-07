3926ce76d99f527fc1dee41b1575829e6b724d62
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-07 02:34:26 +0200

    test: EventQueue continue.

--- src/test/java/com/infoworks/utils/eventq/EventQueueTest.java
+++ src/test/java/com/infoworks/utils/eventq/EventQueueTest.java
@@ -1,5 +1,6 @@
 package com.infoworks.utils.eventq;
 
+import com.infoworks.utils.tasks.AbortTask;
 import com.infoworks.utils.tasks.SimpleTestTask;
 import org.junit.Test;
 
@@ -32,6 +33,37 @@ public class EventQueueTest {
         queue.add(new SimpleTestTask("Hello bro! I am Hayes"));
         queue.add(new SimpleTestTask("Hi there! I am Cris"));
         queue.add(new SimpleTestTask("Let's bro! I am James"));
+        //
+        try {
+            latch.await();
+        } catch (InterruptedException e) {}
+    }
+
+    @Test
+    public void eventConcurrentQueueTest() {
+        //Initialize:
+        CountDownLatch latch = new CountDownLatch(1);
+        AtomicInteger counter = new AtomicInteger(8);
+        //
+        EventQueue queue = new EventQueue(Executors.newFixedThreadPool(3));
+        queue.onTaskComplete((message, state) -> {
+            System.out.println("State: " + state.name());
+            System.out.println(message.toString());
+            if (counter.get() > 1) {
+                counter.decrementAndGet();
+            } else {
+                latch.countDown();
+            }
+        });
+        //Adding Into Queue:
+        queue.add(new SimpleTestTask("Wow bro! I am Adams"));
+        queue.add(new SimpleTestTask("Hello bro! I am Hayes"));
+        queue.add(new AbortTask("Hi there! I am aborting 1"));
+        queue.add(new SimpleTestTask("Hi there! I am Cris"));
+        queue.add(new SimpleTestTask("Let's bro! I am James"));
+        queue.add(new AbortTask("Hi there! I am aborting 1"));
+        queue.add(new SimpleTestTask("Hi there! I am Cris 2"));
+        queue.add(new SimpleTestTask("Hello bro! I am Hayes 2"));
         //
         try {
             latch.await();
