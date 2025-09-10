991602872a644081b30e0e35aab7995ec5ae00ff
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-10 17:39:38 +0200

    test: ObjectMapper test.

--- src/test/java/com/infoworks/orm/ObjectMapperTest.java
+++ src/test/java/com/infoworks/orm/ObjectMapperTest.java
@@ -10,7 +10,10 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.*;
 
@@ -23,6 +26,17 @@ public class ObjectMapperTest {
         assertTrue(persons.isEmpty());
     }
 
+    private static String QUERY = "SELECT uuid,name,age,gender,email FROM Person;";
+
+    private static String[] PersonColNames = {"uuid", "name", "age", "gender", "email"};
+    private static String[] PersonInsertQuery = {
+            "INSERT INTO Person (uuid,name,age,gender,email) VALUES ('00992334','Cris',23,'MALE','Cris@g.com');"
+            , "INSERT INTO Person (uuid,name,age,gender,email) VALUES ('00342334','Adams',32,'MALE','Adams@g.com');"
+            , "INSERT INTO Person (uuid,name,age,gender,email) VALUES ('00242334','James',21,'MALE','James@g.com');"
+            , "INSERT INTO Person (uuid,name,age,gender,email) VALUES ('00122334','Hayes',44,'MALE','Hayes@g.com');"
+            , "INSERT INTO Person (uuid,name,age,gender,email) VALUES ('00562334','Andy',36,'MALE','Andy@g.com');"
+    };
+
     @Test
     public void basicJdbcAndObjectMapperTest() throws SQLException {
         //First initiate database with tables:
@@ -30,19 +44,32 @@ public class ObjectMapperTest {
         //Now try to connect the database:testDB and insert seed rows:
         insertSeedData();
         //Now begin the actual test:
-        try(Connection connection = JsqlConnector.createConnection(DriverClass.H2_EMBEDDED, "testDB")) {
-            Assert.assertTrue(connection != null);
-            System.out.println("Connection was created.");
-            //TODO:
+        try(Connection connection = JsqlConnector.createConnection(DriverClass.H2_EMBEDDED, "testDB");
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY)) {
+            //Test Mapping:
+            List<Person> persons = new PersonObjectMapper().objects(rs);
+            assertFalse(persons.isEmpty());
+            persons.forEach(person -> System.out.println(person.toString()));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     private void insertSeedData() throws SQLException{
-        try(Connection connection = JsqlConnector.createConnection(DriverClass.H2_EMBEDDED, "testDB")) {
-            Assert.assertTrue(connection != null);
-            System.out.println("Connection was created.");
+        try(Connection connection = JsqlConnector.createConnection(DriverClass.H2_EMBEDDED, "testDB");
+            Statement stmt = connection.createStatement()) {
+            //
+            AtomicInteger insertCount = new AtomicInteger(0);
+            Arrays.stream(PersonInsertQuery).forEach(query -> {
+                try {
+                    int result = stmt.executeUpdate(query);
+                    insertCount.incrementAndGet();
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            });
+            System.out.println("Inserted records into the table, Count: " + insertCount.get());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
