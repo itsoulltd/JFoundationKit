1a06eaa7dabd374098d98e0cefe402c006fdf13f
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-10 15:38:04 +0200

    test: Adding basic ObjectMapping Test

--- src/test/java/com/infoworks/orm/ObjectMapperTest.java
+++ src/test/java/com/infoworks/orm/ObjectMapperTest.java
@@ -1,8 +1,13 @@
 package com.infoworks.orm;
 
 import com.infoworks.data.impl.Person;
+import com.infoworks.db.JsqlConnector;
+import com.it.soul.lab.connect.DriverClass;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -16,6 +21,31 @@ public class ObjectMapperTest {
         ResultSet set = null; //Select * From Person;
         List<Person> persons = new PersonObjectMapper().objects(set);
         assertTrue(persons.isEmpty());
+    }
+
+    @Test
+    public void basicJdbcAndObjectMapperTest() throws SQLException {
+        //First initiate database with tables:
+        JsqlConnector.executeScripts(DriverClass.H2_EMBEDDED, "testDB", new File("testDB.sql"));
+        //Now try to connect the database:testDB and insert seed rows:
+        insertSeedData();
+        //Now begin the actual test:
+        try(Connection connection = JsqlConnector.createConnection(DriverClass.H2_EMBEDDED, "testDB")) {
+            Assert.assertTrue(connection != null);
+            System.out.println("Connection was created.");
+            //TODO:
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void insertSeedData() throws SQLException{
+        try(Connection connection = JsqlConnector.createConnection(DriverClass.H2_EMBEDDED, "testDB")) {
+            Assert.assertTrue(connection != null);
+            System.out.println("Connection was created.");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
