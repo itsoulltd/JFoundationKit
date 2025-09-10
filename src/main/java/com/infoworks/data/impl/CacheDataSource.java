cb79feb4dec46306611a14568ab62858e2beb212
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-10 20:58:25 +0200

    feat: Adding CacheDataSource

---
+++ src/main/java/com/infoworks/data/impl/CacheDataSource.java
@@ -0,0 +1,105 @@
+package com.infoworks.data.impl;
+
+import com.infoworks.data.base.iDataSource;
+import com.infoworks.data.cache.LRUCache;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Eviction Policy: Least Recently Used.
+ * Concurrency: Multiple Thread Should Perform operation on the container.
+ * Following has no impact on eviction or LRU-Policy based Key-Sorting:
+ * -fetch(...) OR -readSync(...) OR -readAsync(...)
+ * Cache eviction and LRU-Policy based Key-Sorting applied on following:
+ * -add(...) OR -put(...)
+ * -read(...)
+ * -replace(...) AND -remove(...) OR -delete(...)
+ * @param <E>
+ */
+public class CacheDataSource<E> extends SimpleDataSource<String, E> {
+
+    private final LRUCache<String, E> cacheStorage;
+
+    public CacheDataSource(int maxSize) {
+        this.cacheStorage = new LRUCache<>(maxSize);
+    }
+
+    protected Collection<E> getCacheStorage() {
+        return cacheStorage.values();
+    }
+
+    @Override
+    protected Map<String, E> getInMemoryStorage() {
+        return cacheStorage;
+    }
+
+    @Override
+    public void clear(){
+        if (size() > 0){
+            cacheStorage.clear();
+        }
+    }
+
+    public List<E> fetch(int offset, int pageSize) {
+        int size = size();
+        if (pageSize > size || pageSize <= 0) pageSize = size;
+        return readSyncAsList(offset, pageSize);
+    }
+
+    private List<E> readSyncAsList(int offset, int pageSize) {
+        int size = size();
+        int maxItemCount = Math.abs(offset) + Math.abs(pageSize);
+        if (maxItemCount <= size) {
+            List<E> values = new ArrayList<>(getCacheStorage());
+            List<E> items = values.subList(Math.abs(offset), maxItemCount);
+            return items;
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public void put(String key, E e) {
+        synchronized (cacheStorage) {
+            cacheStorage.put(key, e);
+        }
+    }
+
+    @Override
+    public E remove(String key) {
+        synchronized (cacheStorage) {
+            return cacheStorage.remove(key);
+        }
+    }
+
+    @Override
+    public String add(E e) {
+        String key = String.valueOf(e.hashCode());
+        put(key, e);
+        return key;
+    }
+
+    public iDataSource<String, E> add(E...items){
+        for (E dh: items) add(dh);
+        return this;
+    }
+
+    @Override
+    public void delete(E e) {
+        remove(String.valueOf(e.hashCode()));
+    }
+
+    public iDataSource<String, E> delete(E...items){
+        for (E dh: items) delete(dh);
+        return this;
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return containsKey(String.valueOf(e.hashCode()));
+    }
+
+}
