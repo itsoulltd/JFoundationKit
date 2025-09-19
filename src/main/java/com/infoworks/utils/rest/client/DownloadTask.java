5ad225680929908a4a4027803f307658a025e6e0
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-19 13:59:39 +0200

    fix: DownloadTask and UploadTask added. Need to test.

--- src/main/java/com/infoworks/utils/rest/client/DownloadTask.java
+++ src/main/java/com/infoworks/utils/rest/client/DownloadTask.java
@@ -1,12 +1,19 @@
 package com.infoworks.utils.rest.client;
 
+import com.infoworks.objects.MediaType;
 import com.infoworks.objects.Message;
 import com.infoworks.objects.Response;
-import com.infoworks.objects.Responses;
 import com.infoworks.utils.services.iResources;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpHeaders;
 import java.io.InputStream;
 import java.util.Base64;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class DownloadTask extends GetTask {
@@ -23,41 +30,53 @@ public class DownloadTask extends GetTask {
 
     @Override
     public ResourceResponse execute(Message message) throws RuntimeException {
-        /*RestTemplate template = getClient();
+        ResourceResponse outcome = (ResourceResponse) new ResourceResponse().setStatus(500);
+        //Prepare request builder:
+        HttpRequest.Builder builder = HttpRequest.newBuilder()
+                .uri(URI.create(getUri()))
+                .GET();
+        //Prepare Http-Headers:
+        Map<String, String> headers = createHeaderFrom(getToken());
+        headers.put("User-Agent", "JavaHttpClient/11");
+        headers.put("Accept", "*/*");
+        headers.forEach(builder::header);
+        //GET the file-download:
         try {
-            ResponseEntity<Resource> response = (getParams().length > 0)
-                    ? template.exchange(getUri(), HttpMethod.GET, getBody(), Resource.class, getParams())
-                    : template.exchange(getUri(), HttpMethod.GET, getBody(), Resource.class);
-            if (getResponseListener() != null) {
-                String base64Encoded = null;
-                if (response.hasBody()) {
-                    try (InputStream iso = response.getBody().getInputStream()) {
-                        if (iso != null) {
-                            iResources service = iResources.create();
-                            byte[] bytes = service.readAsBytes(iso);
-                            base64Encoded = new String(Base64.getEncoder().encode(bytes), "UTF-8");
-                        }
+            HttpClient client = getClient();
+            HttpResponse<InputStream> response = client.send(builder.build(), HttpResponse.BodyHandlers.ofInputStream());
+            HttpHeaders responseHeaders = response.headers();
+            outcome.setHeaders(responseHeaders);
+            int statusCode = response.statusCode();
+            if (statusCode >= 200 && statusCode < 300) {
+                outcome.setResource(response.body());
+                //Now convert into base64 string and accept-consumer:
+                if (getResponseListener() != null && response.body() != null) {
+                    String base64Encoded = null;
+                    try (InputStream iso = response.body()) {
+                        iResources service = iResources.create();
+                        byte[] bytes = service.readAsBytes(iso);
+                        base64Encoded = new String(Base64.getEncoder().encode(bytes), "UTF-8");
                     }
+                    getResponseListener().accept(base64Encoded);
                 }
-                getResponseListener().accept(base64Encoded);
+            } else {
+                outcome.setError("Failed to download file. HTTP status code: " + statusCode
+                        + "\nResponse headers: " + responseHeaders.map());
             }
-            return (ResourceResponse) new ResourceResponse()
-                    .setResource(response.getBody())
-                    .setStatus(200)
-                    .setMessage(getUri());
-        } catch (Exception e) {
-            return (ResourceResponse) new ResourceResponse()
-                    .setResource(null)
-                    .setStatus(500)
-                    .setMessage(getUri())
-                    .setError(e.getMessage());
-        }*/
-        //TODO:
-        return (ResourceResponse) new ResourceResponse().setStatus(500);
+        } catch (IOException e) {
+            outcome.setError(e.getMessage());
+        } catch (InterruptedException e) {
+            outcome.setError(e.getMessage());
+        }
+        return outcome;
     }
 
     public static class ResourceResponse extends Response {
         private InputStream resource;
+        private HttpHeaders headers;
+        private long contentLength = 0l;
+        private String filename;
+        private MediaType mediaType;
 
         public InputStream getResource() {
             return resource;
@@ -66,6 +85,41 @@ public class DownloadTask extends GetTask {
         public ResourceResponse setResource(InputStream resource) {
             this.resource = resource;
             return this;
+        }
+
+        public HttpHeaders getHeaders() {
+            return headers;
+        }
+
+        public ResourceResponse setHeaders(HttpHeaders headers) {
+            this.headers = headers;
+            return this;
+        }
+
+        public long getContentLength() {
+            if (getHeaders() != null && this.contentLength == 0l) {
+                getHeaders().firstValue("Content-Length").ifPresent(length -> {
+                    try {
+                        this.contentLength = Long.valueOf(length);
+                    } catch (NumberFormatException e) {}
+                });
+            }
+            return contentLength;
+        }
+
+        public String filename() {
+            if (getHeaders() != null && this.filename == null) {
+                //TODO:
+            }
+            return filename;
+        }
+
+        public MediaType mediaType() {
+            if (getHeaders() != null && this.mediaType == null) {
+                //TODO:
+                this.mediaType = MediaType.BINARY_OCTET_STREAM;
+            }
+            return mediaType;
         }
     }
 }
---
+++ src/main/java/com/infoworks/utils/rest/client/UploadTask.java
@@ -0,0 +1,59 @@
+package com.infoworks.utils.rest.client;
+
+import com.infoworks.objects.MediaType;
+import com.infoworks.objects.Message;
+import com.infoworks.objects.Response;
+import com.infoworks.objects.Responses;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+
+public class UploadTask extends RestTask<Message, Response> {
+
+    private MediaType type;
+    private File uploadFile;
+
+    public UploadTask() {super();}
+
+    public UploadTask(String uploadUri, MediaType type, File uploadFile) {
+        super(uploadUri, "", new Object[0]);
+        this.type = type;
+        this.uploadFile = uploadFile;
+    }
+
+    @Override
+    public Response execute(Message message) throws RuntimeException {
+        Response outcome = new Responses().setStatus(500);
+        if (type == null) return outcome.setError("MediaType cannot be null or empty.");
+        if (uploadFile == null) return outcome.setError("UploadFile cannot be null or empty.");
+        try (FileInputStream inputStream = new FileInputStream(this.uploadFile)) {
+            //Prepare request builder:
+            HttpRequest.Builder builder = HttpRequest.newBuilder()
+                    .uri(URI.create(getUri()))
+                    .header(this.type.key(), this.type.value())
+                    .POST(HttpRequest.BodyPublishers.ofInputStream(() -> inputStream));
+            //Prepare Http-Headers:
+            Map<String, String> headers = createHeaderFrom(getToken());
+            headers.put("User-Agent", "JavaHttpClient/11");
+            headers.forEach(builder::header);
+            //POST file-upload:
+            HttpClient client = getClient();
+            HttpResponse<String> response = client.send(builder.build(), HttpResponse.BodyHandlers.ofString());
+            outcome = new Responses().setStatus(response.statusCode()).setMessage(response.body());
+        } catch (FileNotFoundException e) {
+            outcome.setError(e.getMessage());
+        } catch (IOException e) {
+            outcome.setError(e.getMessage());
+        } catch (InterruptedException e) {
+            outcome.setError(e.getMessage());
+        }
+        return outcome;
+    }
+}
