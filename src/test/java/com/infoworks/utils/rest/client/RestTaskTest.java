47936616c07b395f003166257db6b52ae7e7155a
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-21 00:45:48 +0200

    test: RestTaskTest done

--- src/test/java/com/infoworks/utils/rest/client/RestTaskTest.java
+++ src/test/java/com/infoworks/utils/rest/client/RestTaskTest.java
@@ -40,6 +40,17 @@ public class RestTaskTest {
     }
 
     @Test
+    public void postTestV2() {
+        RestTask task = new PostTask("https://jsonplaceholder.typicode.com/posts", null);
+        task.setBody(new Row().add("title", "foo")
+                .add("body", "bar")
+                .add("userId", "1"), null);
+        Response response = task.execute(null);
+        System.out.println(response.getStatus());
+        System.out.println(response.getMessage());
+    }
+
+    @Test
     public void putTest() {
         RestTask task = new PutTask(
                 "http://localhost:8080"
@@ -49,12 +60,32 @@ public class RestTaskTest {
     }
 
     @Test
+    public void putTestV2() {
+        RestTask task = new PutTask("https://jsonplaceholder.typicode.com/posts", "/1");
+        task.setBody(new Row().add("id", "1")
+                .add("title", "foo updated title")
+                .add("body", "bar updated body")
+                .add("userId", "1"), null);
+        Response response = task.execute(null);
+        System.out.println(response.getStatus());
+        System.out.println(response.getMessage());
+    }
+
+    @Test
     public void deleteTest() {
         RestTask task = new DeleteTask(
                 "http://localhost:8080"
                 , String.format("/api/auth/%s", HttpTask.encodeUrlParam("sohana@gmail.com"))
                 , new Property("full name", "sohana islam"), new Property("email", "sohana@gmail.com"));
         task.execute(null);
+    }
+
+    @Test
+    public void deleteTestV2() {
+        RestTask task = new DeleteTask("https://jsonplaceholder.typicode.com/posts", "/1");
+        Response response = task.execute(null);
+        System.out.println(response.getStatus());
+        System.out.println(response.getMessage());
     }
 
 }
