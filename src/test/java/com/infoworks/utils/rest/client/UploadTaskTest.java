7c29f1cb962d33aa5e625ae51baf182d003ca344
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-21 00:57:48 +0200

    test: UploadTaskTest ...

--- src/test/java/com/infoworks/utils/rest/client/UploadTaskTest.java
+++ src/test/java/com/infoworks/utils/rest/client/UploadTaskTest.java
@@ -5,16 +5,19 @@ import com.infoworks.objects.Response;
 import org.junit.Test;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
 
 public class UploadTaskTest {
 
-    //@Test
+    @Test
     public void uploadTest() {
-        UploadTask task = new UploadTask("http://localhost:8080/api/upload"
-                , MediaType.PNG
-                , new File("/data/final-architecture.png"));
+        Path path = Paths.get("src","test","resources", "data", "final-architecture.png");
+        File imfFile = new File(path.toFile().getAbsolutePath());
+        //
+        UploadTask task = new UploadTask("http://localhost:8080/api/upload", MediaType.PNG, imfFile);
         task.setToken("my-token");
         Response response = task.execute(null);
         System.out.println(response.getStatus());
