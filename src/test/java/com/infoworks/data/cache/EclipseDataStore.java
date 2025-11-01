3936f053b483f51514703735e8b4c2a59bc9ef83
DEGRUYTER\towhid.islam <towhid.islam@degruyterbrill.com>
2025-11-01 13:16:02 +0100

    test: testing EclipseDataStore

---
+++ src/test/java/com/infoworks/data/cache/EclipseDataStore.java
@@ -0,0 +1,160 @@
+package com.infoworks.data.cache;
+
+import com.infoworks.data.base.iDataStore;
+import com.infoworks.data.impl.SimpleDataSource;
+import org.eclipse.serializer.afs.types.ADirectory;
+import org.eclipse.serializer.reference.Lazy;
+import org.eclipse.serializer.reference.LazyReferenceManager;
+import org.eclipse.store.storage.embedded.types.EmbeddedStorage;
+import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
+
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
+public class EclipseDataStore<Key, Value> extends SimpleDataSource<Key, Value> implements iDataStore, AutoCloseable {
+
+    private static Logger LOG = Logger.getLogger(EclipseDataStore.class.getSimpleName());
+    private final EmbeddedStorageManager storage;
+    private final boolean enableLazyLoad;
+    private String location;
+    private RootObject<Key, Value> rootObject;
+    private ExecutorService executors;
+
+    public EclipseDataStore(EmbeddedStorageManager storage, boolean enableLazyLoad, Duration lazyEvictTimeout) {
+        this.enableLazyLoad = enableLazyLoad;
+        if (enableLazyLoad && !lazyEvictTimeout.isZero() && !lazyEvictTimeout.isNegative()) {
+            LazyRootObject.setLazyRefManager(lazyEvictTimeout);
+            LOG.info("Setting up custom LazyReferenceManager was successful.");
+        }
+        this.storage = storage;
+        boolean restored = retrieve();
+        if (restored) LOG.info("Restore from storage is successful.");
+        else LOG.info("Initialization of storage is successful.");
+    }
+
+    public EclipseDataStore(ADirectory location, boolean enableLazyLoad, Duration lazyEvictTimeout) {
+        this(EmbeddedStorage.start(location), enableLazyLoad, lazyEvictTimeout);
+        this.location = location.toPathString();
+    }
+
+    public EclipseDataStore(ADirectory location, boolean enableLazyLoad) {
+        this(location, enableLazyLoad, Duration.ofMinutes(0));
+    }
+
+    public EclipseDataStore(ADirectory location) { this(location, false); }
+
+    public EclipseDataStore(String location, boolean enableLazyLoad, Duration lazyEvictTimeout) {
+        this(EmbeddedStorage.start(Paths.get(location)), enableLazyLoad, lazyEvictTimeout);
+        this.location = location;
+    }
+
+    public EclipseDataStore(String location, boolean enableLazyLoad) {
+        this(location, enableLazyLoad, Duration.ofMinutes(0));
+    }
+
+    public EclipseDataStore(String location) { this(location, false); }
+
+    @Override
+    protected Map<Key, Value> getInMemoryStorage() {
+        return rootObject.getMemStorage();
+    }
+
+    protected ExecutorService getExecutors() {
+        if (executors == null)
+            executors = Executors.newSingleThreadExecutor();
+        return executors;
+    }
+
+    @Override
+    public String getUuid() {
+        return location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    @Override
+    public void save(boolean async) {
+        if(async) getExecutors().submit(() -> storage.store(rootObject.getMemStorage()));
+        else storage.store(rootObject.getMemStorage());
+    }
+
+    @Override
+    public boolean retrieve() {
+        if (Objects.nonNull(rootObject)) return false;
+        if (storage.root() == null) {
+            rootObject = enableLazyLoad ? new LazyRootObject(getLocation()) : new RootObject<>(getLocation());
+            storage.setRoot(rootObject);
+            storage.storeRoot();
+            return false;
+        } else {
+            rootObject = (RootObject<Key, Value>) storage.root();
+            return true;
+        }
+    }
+
+    @Override
+    public boolean delete() {
+        clear();
+        save(false);
+        return true;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (storage.isRunning()) {
+            save(false);
+            storage.shutdown();
+        }
+    }
+
+    ///////////////////////////////////////////Root-Object//////////////////////////////////////////////
+
+    private static class RootObject<Key, Value> {
+
+        private final String location;
+        private final Map<Key, Value> memStorage;
+
+        public RootObject(String location) {
+            this.location = location;
+            this.memStorage = new ConcurrentHashMap<>();
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public Map<Key, Value> getMemStorage() {
+            return memStorage;
+        }
+    }
+
+    private static class LazyRootObject<Key, Value> extends RootObject<Key, Value> {
+
+        private final Lazy<Map<Key, Value>> memStorage;
+
+        public LazyRootObject(String location) {
+            super(location);
+            this.memStorage = Lazy.Reference(new ConcurrentHashMap<>());
+        }
+
+        public Map<Key, Value> getMemStorage() {
+            return Lazy.get(memStorage);
+        }
+
+        public static void setLazyRefManager(Duration lazyEvictTimeout) {
+            LazyReferenceManager.set(LazyReferenceManager.New(
+                    Lazy.Checker(lazyEvictTimeout.toMillis(), 0.75)
+            ));
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+}
---
+++ src/test/java/com/infoworks/data/cache/EclipseDataStoreTest.java
@@ -0,0 +1,195 @@
+package com.infoworks.data.cache;
+
+import com.infoworks.data.impl.Person;
+import org.eclipse.serializer.afs.types.ADirectory;
+import org.eclipse.serializer.persistence.binary.jdk8.types.BinaryHandlersJDK8;
+import org.eclipse.store.afs.blobstore.types.BlobStoreFileSystem;
+import org.eclipse.store.afs.nio.types.NioFileSystem;
+import org.eclipse.store.afs.redis.types.RedisConnector;
+import org.eclipse.store.afs.sql.types.SqlConnector;
+import org.eclipse.store.afs.sql.types.SqlFileSystem;
+import org.eclipse.store.afs.sql.types.SqlProviderPostgres;
+import org.eclipse.store.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
+import org.eclipse.store.storage.embedded.types.EmbeddedStorageFoundation;
+import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
+import org.eclipse.store.storage.exceptions.StorageExceptionInitialization;
+import org.junit.Assert;
+import org.junit.Test;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.time.Duration;
+import java.util.Locale;
+
+public class EclipseDataStoreTest {
+
+    @Test
+    public void initTest() throws Exception {
+        String storagePath = "target/EclipseStore/EclipseDataStoreTest";
+        //Single Cons:
+        EclipseDataStore<String, Person> mData = new EclipseDataStore<>(storagePath + "/One");
+        mData.close();
+        //Double Cons: [Will Not Set]
+        EclipseDataStore<String, Person> mData2 = new EclipseDataStore<>(storagePath + "/Two", true);
+        mData2.close();
+        //Three Cons: [Will Not Set]
+        EclipseDataStore<String, Person> mData3 = new EclipseDataStore<>(storagePath + "/Three", true, Duration.ofMinutes(0));
+        mData3.close();
+        //Same: [Will Not Set]
+        EclipseDataStore<String, Person> mData4 = new EclipseDataStore<>(storagePath + "/Four", true, Duration.ofMinutes(-1));
+        mData4.close();
+        //Same: [Will Not Set]
+        EclipseDataStore<String, Person> mData5 = new EclipseDataStore<>(storagePath + "/Five", false, Duration.ofMinutes(20));
+        mData5.close();
+        //Three Cons: [Will Be Set]
+        EclipseDataStore<String, Person> mData6 = new EclipseDataStore<>(storagePath + "/Six", true, Duration.ofMinutes(1));
+        mData6.close();
+    }
+
+    /**
+     * https://docs.microstream.one/manual/storage/application-life-cycle.html
+     * The consequence of this is:
+     * If two EmbeddedStorageManager instances are started,
+     * each one with a different location for its persistend data,
+     * then the application has two live databases! If three or ten or 100 are started,
+     * then that’s the number of live databases the application has.
+     * There is no limit and no conflict between different databases inside the same application process.
+     * ****************
+     * The only important thing is that no two running StorageManagers can access the same data location.
+     * ****************
+     */
+    @Test(expected = StorageExceptionInitialization.class)
+    public void TwoInstanceToSameStore() {
+        String storagePath = "target/EclipseStore/EclipseDataStoreTest";
+        //
+        EclipseDataStore<String, Person> mData = new EclipseDataStore<>(storagePath + "/TwoInstance");
+        mData.put("james", new Person().setName("james").setAge(29));
+        mData.save(false);
+        //
+        EclipseDataStore<String, Person> mData2 = new EclipseDataStore<>(storagePath + "/TwoInstance");
+        Person person = mData2.read("james");
+        Assert.assertNotNull(person);
+    }
+
+    @Test
+    public void simpleStoreTest() throws Exception {
+        String storagePath = "target/EclipseStore/EclipseDataStoreTest";
+        //
+        EclipseDataStore<String, Person> mData = new EclipseDataStore<>(storagePath + "/SimpleStore");
+        mData.put("james", new Person().setName("james").setEmail("james@gmail.com").setAge(29));
+        //mData.save(false);
+        mData.close(); //close() -> also do save(...) then shutdown().
+        //
+        //After closing a running db, we can re-open and access any data:
+        //
+        EclipseDataStore<String, Person> mData2 = new EclipseDataStore<>(storagePath + "/SimpleStore");
+        Person person = mData2.read("james");
+        Assert.assertNotNull(person);
+        System.out.println(person.getName());
+        mData2.close();
+    }
+
+    @Test
+    public void localFileSystem() {
+        NioFileSystem fileSystem = NioFileSystem.New();
+        ADirectory directory = fileSystem.ensureDirectoryPath(
+                "target", "EclipseStore", "EclipseDataStoreTest", "SimpleStore");
+        //Create storage manager:
+        //EmbeddedStorageManager storageManager = EmbeddedStorage.start(directory);
+        //Using EclipseDataStore: Create and Save:-
+        try (EclipseDataStore<String, Person> mData = new EclipseDataStore<>(directory)) {
+            mData.put("gosling"
+                    , new Person().setName("James Gosling")
+                            .setEmail("james.gosling@gmail.com").setAge(70)
+            );
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        //Second Reload and Test:-
+        try (EclipseDataStore<String, Person> mData = new EclipseDataStore<>(directory)) {
+            Person person = mData.read("gosling");
+            Assert.assertNotNull(person);
+            System.out.println(person.getName());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //@Test
+    public void localFileSystem_using_serializer_jdk_8() {
+        EmbeddedStorageFoundation<?> foundation = EmbeddedStorageConfiguration.Builder()
+                .setStorageDirectory("data/storage")
+                .setChannelCount(Math.max(
+                        1, // minimum one channel, if only 1 core is available
+                        Integer.highestOneBit(Runtime.getRuntime().availableProcessors() - 1)
+                ))
+                .createEmbeddedStorageFoundation();
+
+        foundation.onConnectionFoundation(BinaryHandlersJDK8::registerJDK8TypeHandlers);
+        //Create storage manager:
+        EmbeddedStorageManager storageManager = foundation.createEmbeddedStorageManager().start();
+        try (EclipseDataStore store = new EclipseDataStore(storageManager, true, Duration.ofMillis(1000))) {
+            //TODO:
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //@Test
+    public void blobFileSystem_redis() {
+        String redisUri = "redis://localhost:6379/0";
+        BlobStoreFileSystem fileSystem = BlobStoreFileSystem.New(RedisConnector.Caching(redisUri));
+        ADirectory directory = fileSystem.ensureDirectoryPath("storage");
+        //Create storage manager:
+        //EmbeddedStorageManager storageManager = EmbeddedStorage.start(directory);
+        try (EclipseDataStore store = new EclipseDataStore(directory)) {
+            //TODO:
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //@Test
+    public void sqlFileSystem() {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/mydb");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("secret");
+        SqlFileSystem fileSystem = SqlFileSystem.New(SqlConnector.Caching(SqlProviderPostgres.New(dataSource)));
+        ADirectory directory = fileSystem.ensureDirectoryPath("storage");
+        //Create storage manager:
+        //EmbeddedStorageManager storageManager = EmbeddedStorage.start(directory);
+        try (EclipseDataStore store = new EclipseDataStore(directory)) {
+            //TODO:
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //@Test
+    /*public void blobFileSystem_s3() {
+        S3Client client = S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create("ACCESS_KEY", "SECRET_ACCESS_KEY")
+                ))
+                .region(Region.EU_NORTH_1)
+                .build();
+        BlobStoreFileSystem fileSystem = BlobStoreFileSystem.New(S3Connector.Caching(client));
+        ADirectory directory = fileSystem.ensureDirectoryPath("bucket-name", "folder", "subfolder");
+        //Create storage manager:
+        //EmbeddedStorageManager storageManager = EmbeddedStorage.start(directory);
+        EclipseDataStore store = new EclipseDataStore(directory);
+    }*/
+
+    //@Test
+    /*public void blobFileSystem_azure() {
+        StorageSharedKeyCredential credential = new StorageSharedKeyCredential("accountName", "accountKey");
+        String endpoint = String.format(Locale.ROOT, "https://%s.blob.core.windows.net", "accountName");
+        BlobServiceClient client = new BlobServiceClientBuilder().endpoint(endpoint).credential(credential).buildClient();
+        BlobStoreFileSystem fileSystem = BlobStoreFileSystem.New(AzureStorageConnector.Caching(client));
+        ADirectory directory = fileSystem.ensureDirectoryPath("storage");
+        //Create storage manager:
+        //EmbeddedStorageManager storageManager = EmbeddedStorage.start(directory);
+        EclipseDataStore store = new EclipseDataStore(directory);
+    }*/
+
+}
