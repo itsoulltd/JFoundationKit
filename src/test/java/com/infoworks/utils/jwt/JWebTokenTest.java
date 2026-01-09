eb1590da06bcc4539b4e7a597ac3e748298c4343
DEGRUYTER\towhid.islam <towhid.islam@degruyterbrill.com>
2026-01-09 20:31:13 +0100

    test: JWebToken test

--- src/test/java/com/infoworks/utils/jwt/JWebTokenTest.java
+++ src/test/java/com/infoworks/utils/jwt/JWebTokenTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class JWebTokenTest {
@@ -20,18 +21,54 @@ public class JWebTokenTest {
                 .setIat(Instant.now().toEpochMilli())
                 .setIss("Towhid")
                 .setSub("m.towhid@gmail.com");
-        String token = jwt.generateToken("What-A-Secret!"
+        String secret = UUID.randomUUID().toString();
+        String token = jwt.generateToken(secret
                 , payload
                 , TokenProvider.timeToLive(Duration.ofMinutes(5), TimeUnit.MINUTES));
         Assert.assertTrue(token != null);
         System.out.println("JWT:" + token);
         //Test-Validation:
-        boolean isValid = jwt.isValid(token, "What-A-Secret!");
+        boolean isValid = jwt.isValid(token, secret);
         Assert.assertTrue(isValid);
         System.out.println("isValid: " + isValid);
     }
 
     @Test
+    public void tokenExpireTest() throws InterruptedException {
+        JWebToken jwt = new JWebToken();
+        JWTPayload payload = new JWTPayload()
+                .setIat(Instant.now().toEpochMilli())
+                .setIss("Towhid")
+                .setSub("m.towhid@gmail.com")
+                .setExp(Instant.now().toEpochMilli());
+        String token = jwt.generateToken("What-A-Secret!", payload, null);
+        Assert.assertTrue(token != null);
+        System.out.println("JWT:" + token);
+        //Wait- 2 sec
+        Thread.sleep(5000);
+        //Test-Validation:
+        boolean isValid = jwt.isValid(token, "What-A-Secret!");
+        Assert.assertTrue(isValid == false);
+        System.out.println("isValid: " + isValid);
+    }
+
+    @Test
+    public void tokenSecretTest() throws InterruptedException {
+        JWebToken jwt = new JWebToken();
+        JWTPayload payload = new JWTPayload()
+                .setIat(Instant.now().toEpochMilli())
+                .setIss("Towhid")
+                .setSub("m.towhid@gmail.com");
+        String token = jwt.generateToken("What-A-Secret!", payload, TokenProvider.timeToLive(Duration.ofMinutes(5), TimeUnit.MINUTES));
+        Assert.assertTrue(token != null);
+        System.out.println("JWT:" + token);
+        //Test-Validation:
+        boolean isValid = jwt.isValid(token, "Ohh-A-Secret!");
+        Assert.assertTrue(isValid == false);
+        System.out.println("isValid: " + isValid);
+    }
+
+    //@Test
     public void jwtPayloadTest() throws IOException {
         JWTPayload payload = new JWTPayload().setIss("Towhid").setSub("m.towhid@gmail.com");
         //
